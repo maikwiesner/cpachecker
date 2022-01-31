70eb36f1a91c93f1446a468670a38a4d0f6b94a0
Maik Wiesner <wiesner@svps.tu-darmstadt.de>
2022-01-31 11:55:03 +0100

    added simple test program

---
+++ test/programs/invariantGeneration/simpleIfElse.c
@@ -0,0 +1,16 @@
+extern int __VERIFIER_nondet_int();
+
+int main() {
+
+  int x = 0;
+  int b = __VERIFIER_nondet_int();
+
+  if (b) {
+      x = 1;
+  } else {
+      x = 3;
+  }
+
+  return x;
+
+}
